d727026eb2bf83d45f2baccc82f362687f926842
realapse-b3trsurv <Vermajonas@gmail.com>
2025-12-27 13:37:22 -0800

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
       "@assets": path.resolve(__dirname, "./src/assets"),
+      "@shared": path.resolve(__dirname, "../shared"),
     },
   },
 });
